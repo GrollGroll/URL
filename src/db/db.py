7c298e84f865f7092fe1bc7ec75bec0e0bff9226
Anton <149895339+GrollGroll@users.noreply.github.com>
2024-03-30 12:17:48 +0300

    Update db.py

--- src/db/db.py
+++ src/db/db.py
@@ -9,12 +9,3 @@ async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         return session
-
-
-# При использовании конструкции из лекции:
-    
-# async def get_session() -> AsyncSession:
-#     async with async_session() as session:
-#         yield session 
-    
-# Ошибка TypeError: object async_generator can't be used in 'await' expression
